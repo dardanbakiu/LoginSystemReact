1219a5a39f700a5e52e137e225c45af833697bbb
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-08-23 23:07:39 +0200

    Backend: test

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use('/', (req,res)=>{
+    const users = [
+        {id:1 , username: 'dardan', password: 'pw'},
+        {id:2 , username: 'mirjeta', password: 'pw'},
+    ]
+
+    res.json(users)
+})
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`BackEnd listening on port ${port}`)
+})
