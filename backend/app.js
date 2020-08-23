be178fa4e8e867189c2e285079ac42d6263415ad
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-08-23 22:15:33 +0200

    backend: folders edited

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
-const express = 
+const express = require('express')
+const app = express()
+
+app.use('/' ,(req,res)=>{
+    let users = [
+        {id:1 , username:'dardanbakiu', password:'pw'},
+        {id:1 , username:'dardanbakiu', password:'pw'}
+    ]
+})
+
+const port = 3000
+app.listen(port)
