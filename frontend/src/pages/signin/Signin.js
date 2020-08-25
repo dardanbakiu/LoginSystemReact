7500271b0724aff478a38bb39d9e4bac0a89636b
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-08-25 20:51:46 +0200

    Frontend: login post request handling using axios

--- frontend/src/pages/signin/Signin.js
+++ frontend/src/pages/signin/Signin.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import Input from '../components/Input'
 import Btn from '../components/Btn'
+import axios from 'axios'
 import '../style/General.css'
 
 export default class Signin extends Component {
+    state = {
+        username:null,
+        password:null
+    }
+
+    onUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onPasswordChange = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    onSignInClick = () => {
+        const url = 'http://localhost:3001'
+        axios.post(`${url}/api/signin/`, {
+            username: this.state.username,
+            password: this.state.password
+        })
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
+    }
 
     render() {
         return (
@@ -17,9 +44,9 @@ export default class Signin extends Component {
 
                 <div >
                     <h1 className="center">SIGN IN</h1>
-                    <Input type="text" label="Username" />
-                    <Input type="password" label='Password' />
-                    <Btn label="Sign In" />
+                    <Input type="text" label="Username" Change={this.onUsernameChange} />
+                    <Input type="password" label='Password' Change={this.onPasswordChange} />
+                    <Btn label="Sign In" Click={this.onSignInClick} />
                 </div>
             </div>
         )
